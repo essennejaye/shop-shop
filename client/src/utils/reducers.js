aa558997312f498e84555019b45ea0beb846307a
essennejaye <sjeffers02@gmail.com>
2021-04-06 23:46:26 -0700

    action tests

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -2,7 +2,13 @@ import { useReducer } from 'react';
 import {
   UPDATE_PRODUCTS,
   UPDATE_CATEGORIES,
-  UPDATE_CURRENT_CATEGORY
+  UPDATE_CURRENT_CATEGORY,
+  ADD_TO_CART,
+  ADD_MULTIPLE_TO_CART,
+  REMOVE_FROM_CART,
+  UPDATE_CART_QUANTITY,
+  CLEAR_CART,
+  TOGGLE_CART
 } from "./actions";
 
 export const reducer = (state, action) => {
@@ -30,6 +36,31 @@ export const reducer = (state, action) => {
       return {
         ...state,
         currentCategory: action.currentCategory,
+      };
+
+    // if action type value is the value of 'ADD_TO_CART',
+    // return a new state with an updated cart
+    case ADD_TO_CART:
+      return {
+        ...state,
+        cartOpen: true,
+        cart: [...state.cart, action.product]
+      };
+
+    case ADD_MULTIPLE_TO_CART:
+      return {
+        ...state,
+        cart: [...state.cart, ...action.products]
+      };
+
+    case REMOVE_FROM_CART:
+      let newState = state.cart.filter(product => {
+        return product._id !== action._id;
+      })
+      return {
+        ...state,
+        cartOpen: newState.length > 0,
+        cart: newState
       };
 
     // if it's none of these actions, do not update state at all and keep things the same!
